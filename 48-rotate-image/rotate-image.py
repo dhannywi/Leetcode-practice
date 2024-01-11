5158f0fe851d3c358784b313e7d651c35dd1b198
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-10 20:04:52 -0600

    Time: 37 ms (85.22%) | Memory: 17.4 MB (6.75%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -8,8 +8,20 @@ class Solution:
         #         matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         # for lst in matrix:
         #     lst.reverse()
-        matrix.reverse()
-        for r in range(len(matrix)):
+        # matrix.reverse()
+        # for r in range(len(matrix)):
+        #     for c in range(r):
+        #         matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        m_len = len(matrix)
+        left = 0
+        right = m_len - 1
+        while left < right:
+            matrix[left], matrix[right] = matrix[right], matrix[left]
+            left += 1
+            right -= 1
+        
+        for r in range(m_len):
             for c in range(r):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
                 
