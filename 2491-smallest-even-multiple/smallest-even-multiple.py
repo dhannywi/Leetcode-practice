f50fe2f32412e4221714a158b33180f56884cb3c
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-13 15:39:17 -0600

    Time: 34 ms (76.87%) | Memory: 17.4 MB (7.29%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.py
+++ 2491-smallest-even-multiple/smallest-even-multiple.py
@@ -1,5 +1,4 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        # smallest multiple of 2 and n
-        return n if n%2==0 else n*2
+        return n if n % 2 == 0 else n * 2
         
