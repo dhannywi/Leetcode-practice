8cb6aaf1e8533288d0e1f1c7a1b6f9dfacbf5bbd
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-14 17:37:40 -0600

    Time: 47 ms (68.08%) | Memory: 23.4 MB (6.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # use only alphanumeric chars in string O(n)
-        s_alnum = ''
-        for c in s:
-            if c.isalnum():
-                s_alnum += c.lower()
+        # s_alnum = ''
+        # for c in s:
+        #     if c.isalnum():
+        #         s_alnum += c.lower()
         # two pointer approach
         # left = 0
         # right = len(s_alnum) - 1
@@ -15,4 +15,6 @@ class Solution:
         #         left += 1
         #         right -= 1
         # return True
+
+        s_alnum = [i.lower() for i in s if i.isalnum()]
         return s_alnum == s_alnum[::-1]
