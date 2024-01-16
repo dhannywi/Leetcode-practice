0078d30813f784fcc5ef0a49a56fad79a9b12cb5
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-16 13:16:17 -0600

    Time: 32 ms (96.90%) | Memory: 17.4 MB (10.55%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # --- array slicing O(n+m log n+m) time
-        nums1[m:] = nums2[:]
-        nums1.sort() 
+        # # --- array slicing O(n+m log n+m) time
+        # nums1[m:] = nums2[:]
+        # nums1.sort() 
         
+        m, n, last = m-1, n-1, m+n-1
+
+        while n >= 0:
+            if m >= 0 and nums1[m] > nums2[n]:
+                nums1[last] = nums1[m]
+                m -= 1
+            else:
+                nums1[last] = nums2[n]
+                n -= 1
+            last -= 1
+
+            
