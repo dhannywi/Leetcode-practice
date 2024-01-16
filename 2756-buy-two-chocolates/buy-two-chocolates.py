4e33daf11924946000be0e5afaf35d68f0cfe08f
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-16 14:08:33 -0600

    Time: 61 ms (65.52%) | Memory: 17.3 MB (23.50%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,5 +1,19 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
+        min1 = min2 = 101
+        for i in prices:
+            if i < min1:
+                min1, min2 = i, min1
+            elif i < min2:
+                min2 = i
+
+        leftover = money - min1 - min2
+        return leftover if leftover >= 0 else money
+
+
+        '''
+        # time: O(n logn) space: O(1)
         prices.sort()
         leftover = money - prices[0] - prices[1]
         return leftover if leftover >= 0 else money
+        '''
