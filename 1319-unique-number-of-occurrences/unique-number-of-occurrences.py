93f3a863cd5276e8c529e9ca6b99137e9bf223d3
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-16 18:17:11 -0600

    Time: 46 ms (44.70%) | Memory: 17.4 MB (25.02%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,9 +1,7 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         # create dict with count O(n)
-        freq = {}
-        for i in arr:
-            freq[i] = freq.get(i, 0) + 1
-        
+        freq = Counter(arr)
+
         return len(freq) == len(set(freq.values()))
             
