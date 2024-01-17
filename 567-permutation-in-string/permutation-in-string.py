65c29db71e1ba04a06b03453a2e28d140c21a18c
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-17 10:23:19 -0600

    Time: 1007 ms (21.20%) | Memory: 17.4 MB (42.65%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,12 +4,11 @@ class Solution:
         pS1 = {}
         for i in s1:
             pS1[i] = pS1.get(i, 0) + 1
-        # window size
-        w = len(s1)
+        # window size and right pointer
+        w = r = len(s1)
         # store char count of s2 in a window
         pS2 = Counter()
-        r = w
-        for i in range(len(s2)- w+1): # O(n+w)
+        for i in range(len(s2) - w+1): # O(n+w)
             pS2 = Counter(s2[i:r])
             if pS2 == pS1:
                 return True
