b26296d9d4b7667737d30ae03e49b00717a7557f
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-17 10:26:28 -0600

    Time: 1020 ms (21.02%) | Memory: 17.4 MB (26.85%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,13 +1,10 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # store char count of s1
-        pS1 = {}
-        for i in s1:
-            pS1[i] = pS1.get(i, 0) + 1
+        pS1 = Counter(s1)
         # window size and right pointer
         w = r = len(s1)
-        # store char count of s2 in a window
-        pS2 = Counter()
+        # store char count of s2 current window
         for i in range(len(s2) - w+1): # O(n+w)
             pS2 = Counter(s2[i:r])
             if pS2 == pS1:
