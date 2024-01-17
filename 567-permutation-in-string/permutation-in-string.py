36a97b779f9eee48cb9de4068a2fba762c6593a4
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-17 11:36:41 -0600

    Time: 91 ms (42.66%) | Memory: 17.5 MB (26.85%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,16 +1,25 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # store char count of s1
+        # window size 
+        w = len(s1)
+        # store char count of s1 & count of s2 char in 1st window
         pS1 = Counter(s1)
-        # window size and right pointer
-        w = r = len(s1)
-        for i in range(len(s2) - w+1): # O(n+w)
-            # store char count of s2 current window
-            pS2 = Counter(s2[i:r])
+        pS2 = Counter(s2[0:w])
+
+        # check 1st window permutation
+        if pS2 == pS1:
+                return True
+
+        for i in range(w, len(s2)):
+            # Update window by removing the leftmost character and adding the new character
+            lc = s2[i - w]
+            pS2[lc] -= 1
+            if pS2[lc] == 0:
+                del pS2[lc]
+            pS2[s2[i]] += 1
+
+            # check current window permutation
             if pS2 == pS1:
                 return True
-            else:
-                r += 1
-        return False
 
-        
+        return False
