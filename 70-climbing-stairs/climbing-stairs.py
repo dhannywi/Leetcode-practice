7632706e29b6ab76c314234ca1ec76303f076625
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-17 23:18:05 -0600

    Time: 28 ms (95.13%) | Memory: 17.2 MB (31.72%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,6 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         # super optimized solution
         # time: O(n), space: O(1)
+        if n <= 3:
+            return n
+            
         a = 1
         b = 2
         for i in range(2, n+1):
