8df2522dba83f190b35f2e87f4fa0ca3b3edf2db
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-01-30 14:07:52 -0600

    Time: 438 ms (77.10%) | Memory: 17.2 MB (69.07%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,17 +1,15 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        # dynamic sliding window
-        max_len = 0
-        z_count = 0
         l = 0
+        flip = 0
+        maxL = 0
         for r in range(len(nums)):
             if nums[r] == 0:
-                z_count += 1
-
-            while z_count > k:
+                flip += 1
+            while flip > k:
                 if nums[l] == 0:
-                    z_count -= 1
+                    flip -= 1
                 l += 1
-            max_len = max(max_len, r-l+1)
-        return max_len
+            maxL = max(maxL, r-l+1)
+        return maxL
 
