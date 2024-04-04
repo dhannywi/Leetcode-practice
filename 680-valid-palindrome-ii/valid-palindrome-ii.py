ab14457c4d6c5d8b339c33b5cbcc406c50896b5e
Dhanny Indrakusuma <73628243+dhannywi@users.noreply.github.com>
2024-04-04 18:03:57 -0500

    Time: 107 ms (46.02%) | Memory: 17 MB (67.96%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,12 +1,22 @@
 class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        l = 0
-        r = len(s) -1
+    def is_palindrome(self, l, r, s):
+        # check if string is palindrome
         while l < r:
             if s[l] != s[r]:
-                skipL, skipR = s[l+1:r+1], s[l:r]
-                return (skipL == skipL[::-1]) or (skipR == skipR[::-1])
+                return False
             l += 1
             r -= 1
         return True
+    
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l] != s[r]:
+                return self.is_palindrome(l+1, r, s) or self.is_palindrome(l, r-1, s)
+            l += 1
+            r -= 1
+        
+        return True
 
+# "aba"
+# a==a
